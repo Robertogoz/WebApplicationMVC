62b60d2fff4c33ff5855752eff52d80d25b5dfe8
robertogoz <gozjunior@hotmail.com>
2022-04-26 07:56:31 -0300

    feat(database): register context in program and initialize db

--- WebApplicationMVC/Models/Course.cs
+++ WebApplicationMVC/Models/Course.cs
@@ -6,7 +6,7 @@ namespace WebApplicationMVC.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ID { get; set; }
-        public string title { get; set; }
+        public string Title { get; set; }
         public int Credits { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
--- WebApplicationMVC/Program.cs
+++ WebApplicationMVC/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplicationMVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
@@ -11,6 +17,19 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+} else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<AppDataContext>();
+    context.Database.EnsureCreated();
+    DbInitialize.Initialize(context);
 }
 
 app.UseHttpsRedirection();
