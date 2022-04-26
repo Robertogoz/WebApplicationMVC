4c7c072282c4016e95290dd935396e0fbb5f0700
robertogoz <gozjunior@hotmail.com>
2022-04-26 07:18:59 -0300

    feat(model): create Student, Enrollment and Course models

---
+++ WebApplicationMVC/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationMVC.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+        public string title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
