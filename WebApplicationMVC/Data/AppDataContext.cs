51d206ed75756aee92f79307cec92a981f52e821
robertogoz <gozjunior@hotmail.com>
2022-04-26 07:26:40 -0300

    feat(context): create AppDataContext

---
+++ WebApplicationMVC/Data/AppDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Data
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+        { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
