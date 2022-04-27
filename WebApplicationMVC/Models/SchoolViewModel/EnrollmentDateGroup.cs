6327571d52cda144c6193bd16a77f71abaa825f5
robertogoz <gozjunior@hotmail.com>
2022-04-27 07:50:40 -0300

    feat(model): create viewModel EnrollmentDataGroup.cs

---
+++ WebApplicationMVC/Models/SchoolViewModel/EnrollmentDateGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationMVC.Models.SchoolViewModel
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.DateTime)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
