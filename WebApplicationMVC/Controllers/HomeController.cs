3f7e64483ad9159fd35ceda225742cbad83c11ed
robertogoz <gozjunior@hotmail.com>
2022-04-27 07:59:25 -0300

    feat(backend): create an About page

--- WebApplicationMVC/Controllers/HomeController.cs
+++ WebApplicationMVC/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using WebApplicationMVC.Data;
 using WebApplicationMVC.Models;
+using WebApplicationMVC.Models.SchoolViewModel;
+
 
 namespace WebApplicationMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDataContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -27,6 +33,19 @@ namespace WebApplicationMVC.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into DateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = DateGroup.Key,
+                    StudentCount = DateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
