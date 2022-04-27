22bb9c8e2c796f6bbfd999cb6d700c6bacd4e7f1
robertogoz <gozjunior@hotmail.com>
2022-04-27 09:03:02 -0300

    feat(Model): update Student entity using DataAnnotations

--- WebApplicationMVC/Models/Student.cs
+++ WebApplicationMVC/Models/Student.cs
@@ -1,9 +1,18 @@
-﻿namespace WebApplicationMVC.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationMVC.Models {
     public class Student
     {
         public int ID { get; set; }
+
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [StringLength(50)]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment>? Enrollments { get; set; }
